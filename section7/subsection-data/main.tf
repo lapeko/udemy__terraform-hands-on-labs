f7b9b1a16e92ab2d759f3f25ec53c2d3052467b6
lapeko <lapeko88@gmail.com>
2025-08-02 17:01:07 +0200

    lab implementation data and modules blocks

---
+++ section7/subsection-data/main.tf
@@ -0,0 +1,19 @@
+data "aws_ami" "ubuntu_20" {
+  most_recent = true
+
+  owners = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+output "ubuntu_20_id" {
+  value = data.aws_ami.ubuntu_20.id
+}
---
+++ section7/subsection-modules/main.tf
@@ -0,0 +1,31 @@
+module "subnet_addrs" {
+  source = "hashicorp/subnets/cidr"
+
+  base_cidr_block = "10.0.0.0/8"
+  networks = [
+    {
+      name     = "foo"
+      new_bits = 8
+    },
+    {
+      name     = "bar"
+      new_bits = 8
+    },
+    {
+      name     = "baz"
+      new_bits = 4
+    },
+    {
+      name     = "beep"
+      new_bits = 8
+    },
+    {
+      name     = "boop"
+      new_bits = 8
+    },
+  ]
+}
+
+output "subnet_addrs" {
+  value = module.subnet_addrs.network_cidr_blocks
+}
